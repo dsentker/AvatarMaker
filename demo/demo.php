2c91c09a407168cc7b67d6cf0b4b78ce6e4c0fe2
Daniel Sentker <tracid2008@googlemail.com>
2016-01-29 08:52:58 +0100

    Updated dependencies

--- demo/demo.php
+++ demo/demo.php
@@ -1,9 +1,8 @@
 <?php
+header('Content-Type: text/html; charset=utf-8');
 ini_set('display_errors', 1);
 error_reporting(-1);
-require_once 'RandomColor.php';
 //require_once 'vendor/autoload.php';
-header('Content-Type: text/html; charset=utf-8');
 
 $manager = new \Intervention\Image\ImageManager(['driver' => 'gd']);
 $avatar = new \Shift\AvatarMaker\AvatarMaker($manager);
@@ -18,7 +17,8 @@ $avatar->setHues(['red', 'orange']);
     <title>Avatar Test</title>
     <style>
         body {
-            background-color: #ccc;
+            background-color: #eee;
+            font-family: sans-serif;
         }
     </style>
 </head>
@@ -26,7 +26,7 @@ $avatar->setHues(['red', 'orange']);
 <?php
 foreach (['John Doe', 'J. D.', 'William D. Smith', 'Megan Fox'] as $name) {
     $img = $avatar->makeAvatar($name)->toBase64();
-    printf('Name: %s<br /><img alt="Avatar" src="%s"/><hr />', $name, $img);
+    printf('<h2>%s</h2><img alt="Avatar" src="%s"/><hr />', $name, $img);
 }
 ?>
 </body>
